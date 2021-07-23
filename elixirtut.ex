7386c3617556c8e6702a8bf5df485a365c9cf4c3
jim Bramnick <jbramnick412@gmail.com>
2021-07-23 19:51:10 -0400

    commit 2

--- elixirtut.ex
+++ elixirtut.ex
@@ -2,6 +2,7 @@ defmodule M do
   def hello do
     name = IO.gets("What is your name?") |> String.trim
     IO.puts "Hello"
+    IO.puts "Hello"
     IO.puts "hello #{name}"
     IO.puts "Hello"
   end
