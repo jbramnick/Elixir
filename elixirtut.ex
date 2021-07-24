1447a51dcec6cde80b039ad4f6ca3a60634cbfb7
jim Bramnick <jbramnick412@gmail.com>
2021-07-23 20:00:23 -0400

    IO made

--- elixirtut.ex
+++ elixirtut.ex
@@ -1,10 +1,11 @@
 defmodule M do
   def hello do
     name = IO.gets("What is your name?") |> String.trim
-    IO.puts "Hello"
-    IO.puts "Hello"
-    IO.puts "Hello2"
     IO.puts "hello #{name}"
-    IO.puts "Hello"
+  end
+end
+defmodule D do
+  def hello2 do
+    IO.puts "hello"
   end
 end
